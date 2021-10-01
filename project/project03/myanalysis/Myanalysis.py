8806ca77ab4b2582b690f8300920019facdabb73
yoonputer <yoonputer@naver.com>
2021-10-02 03:54:39 +0900

    Myanalysis.py using csv -> DB

--- project/project03/myanalysis/Myanalysis.py
+++ project/project03/myanalysis/Myanalysis.py
@@ -1,11 +1,14 @@
 import pandas as pd
-from config.settings import DATA_DIRS
+import sqlite3
+from project.config.settings import DATA_DIRS
 
 
 class Co2:
     # c1 -> 개선 활동
     def c1(self,ind):
-        df = pd.read_csv(DATA_DIRS[0]+'\\sorting.csv', encoding='cp949');
+        connect = sqlite3.connect(DATA_DIRS[0]+'\\sorting.db')
+
+        df = pd.read_sql_query('select * from sorting',connect)
         df2 = df[['industry','act','money','deco2']]
         df3 = df2[df2['industry'] == ind]
 
@@ -14,7 +17,8 @@ class Co2:
 
     # c2 -> 산업명
     def c2(self,ind):
-        df = pd.read_csv(DATA_DIRS[0]+'\\sorting.csv', encoding='cp949');
+        connect = sqlite3.connect(DATA_DIRS[0]+'\\sorting.db')
+        df = pd.read_sql_query('select * from sorting',connect)
         df2 = df[['sort','industry']]
         df3 = df2[df2['industry'] == ind]
 
@@ -23,7 +27,8 @@ class Co2:
 
     # c3 -> 절감액
     def c3(self,ind):
-        df = pd.read_csv(DATA_DIRS[0]+'\\sorting.csv', encoding='cp949');
+        connect = sqlite3.connect(DATA_DIRS[0]+'\\sorting.db')
+        df = pd.read_sql_query('select * from sorting',connect)
         df2 = df[['industry','money']]
         df3 = df2[df2['industry'] == ind]
 
@@ -32,7 +37,8 @@ class Co2:
 
     # c4 -> 감축량
     def c4(self,ind):
-        df = pd.read_csv(DATA_DIRS[0]+'\\sorting.csv', encoding='cp949');
+        connect = sqlite3.connect(DATA_DIRS[0]+'\\sorting.db')
+        df = pd.read_sql_query('select * from sorting',connect)
         df2 = df[['industry','deco2']]
         df3 = df2[df2['industry'] == ind]
 
@@ -41,7 +47,11 @@ class Co2:
 
     # graph -> 그래프용 데이터 추출
     def graph(self,ind,sido):
-        df = pd.read_excel(DATA_DIRS[0]+'\\calc2_result_graph.xlsx',engine='openpyxl', index_col=0);
+        #df = pd.read_excel('../../data/calc2_result_graph.xlsx',engine='openpyxl', index_col=0);
+        connect = sqlite3.connect(DATA_DIRS[0]+'\\sorting.db')
+        df = pd.read_sql_query('select * from calc2_result_graph',connect)
+        df = df.set_index('time')
+
         graph_col = ind + sido
 
         for i in df:
@@ -56,8 +66,8 @@ class Co2:
 
 
 if __name__ == '__main__':
-    # print(Co2().c1('manufacture'))
-    print(Co2().c3('manufacture'))
-    print(Co2().graph('manufacture','강원도'))
+     print(Co2().c1('manufacture'))
+     print(Co2().c3('manufacture'))
+     print(Co2().graph('manufacture','강원도'))
 
 
