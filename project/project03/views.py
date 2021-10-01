58fcf6c3751942c69e73caf31bcda366c835f6c9
solbi <csb1422@naver.com>
2021-10-01 10:10:12 +0900

    BlogDetail

--- project/project03/views.py
+++ project/project03/views.py
@@ -18,41 +18,8 @@ def index(request):
 	return render(request, 'index.html')
 
 def blog(request):
-	# global result
-	category = request.GET.get('category', None)
-	search = request.GET.get('search', None)
 
-	if category == None or search == None:
-		return render(request, 'blog.html')
-	else:
-		df = pd.read_csv('../project/ML/ML_c/mean_ghs.csv', encoding='cp949')
-		cat = {'석유화학': 'chemistry', '음식료품 · 제조업': 'manufacture', '광업': 'mining', '조선': 'ship', '건물': 'building',
-			   '교통': 'transportation', '반도체.디스플레이.전기전자': 'electric',
-			   '발전 · 에너지': 'energy', '유리 · 요업': 'glass', '제지': 'paper'}
-		df['DSGN_INDS'] = df['DSGN_INDS'].map(cat)
-		s = (df['DSGN_INDS'] == category)
-		sort = df[s]
-
-		co2 = sort.GHG_EMS[sort['CORP_NM'] == search].mean()
-		co2_mean = sort['GHG_EMS'].mean()
-		co2_min = sort['GHG_EMS'].min()
-		co2_max = sort['GHG_EMS'].max()
-		co2_all = (sort['GHG_EMS'].mean())*(len(sort['GHG_EMS']))
-		eng = sort.ENG_CNSM[sort['CORP_NM'] == search].mean()
-		eng_mean = sort['ENG_CNSM'].mean()
-		eng_min = sort['ENG_CNSM'].min()
-		eng_max = sort['ENG_CNSM'].max()
-		eng_all = (sort['ENG_CNSM'].mean()) * (len(sort['ENG_CNSM']))
-
-		co2_loc_mean=(co2_mean / co2_all) * 233
-		co2_loc = (co2 / co2_all) * 233
-		eng_loc_mean = (eng_mean / co2_all) * 233
-		eng_loc = (eng / co2_all) * 233
-
-		result = {'category': category, 'search': search, 'co2': co2, 'co2_mean': co2_mean, 'co2_min': co2_min, 'co2_all':co2_all,
-				  'co2_max': co2_max, 'eng': eng, 'eng_mean': eng_mean, 'eng_min': eng_min, 'eng_max': eng_max, 'eng_all':eng_all,
-				  'co2_loc_mean':co2_loc_mean, 'co2_loc':co2_loc, 'eng_loc_mean':eng_loc_mean, 'eng_loc':eng_loc}
-		return render(request, 'blog.html', context=result)
+	return render(request, 'blog.html')
 
 url = 'https://search.naver.com/search.naver?query=%EC%98%A8%EC%8B%A4%EA%B0%80%EC%8A%A4&where=news&ie=utf8&sm=nws_hty'
 req = requests.get(url)
@@ -128,8 +95,44 @@ def elements(request):
 
 	return render(request, 'elements.html')
 def blog_details(request):
+	# global result
+	category = request.GET.get('category', None)
+	search = request.GET.get('search', None)
 
-	return render(request, 'blog_details.html')
+	if category == None or search == None:
+		return render(request, 'blog_details.html')
+	else:
+		df = pd.read_csv('../project/ML/ML_c/mean_ghs.csv', encoding='cp949')
+		cat = {'석유화학': 'chemistry', '음식료품 · 제조업': 'manufacture', '광업': 'mining', '조선': 'ship', '건물': 'building',
+			   '교통': 'transportation', '반도체.디스플레이.전기전자': 'electric',
+			   '발전 · 에너지': 'energy', '유리 · 요업': 'glass', '제지': 'paper'}
+		df['DSGN_INDS'] = df['DSGN_INDS'].map(cat)
+		s = (df['DSGN_INDS'] == category)
+		sort = df[s]
+
+		co2 = sort.GHG_EMS[sort['CORP_NM'] == search].mean()
+		co2_mean = sort['GHG_EMS'].mean()
+		co2_min = sort['GHG_EMS'].min()
+		co2_max = sort['GHG_EMS'].max()
+		co2_all = (sort['GHG_EMS'].mean()) * (len(sort['GHG_EMS']))
+		eng = sort.ENG_CNSM[sort['CORP_NM'] == search].mean()
+		eng_mean = sort['ENG_CNSM'].mean()
+		eng_min = sort['ENG_CNSM'].min()
+		eng_max = sort['ENG_CNSM'].max()
+		eng_all = (sort['ENG_CNSM'].mean()) * (len(sort['ENG_CNSM']))
+
+		co2_loc_mean = (co2_mean / co2_all) * 2330
+		co2_loc = (co2 / co2_all) * 2330
+		eng_loc_mean = (eng_mean / co2_all) * 2330
+		eng_loc = (eng / co2_all) * 2330
+
+		result = {'category': category, 'search': search, 'co2': co2, 'co2_mean': co2_mean, 'co2_min': co2_min,
+				  'co2_all': co2_all,
+				  'co2_max': co2_max, 'eng': eng, 'eng_mean': eng_mean, 'eng_min': eng_min, 'eng_max': eng_max,
+				  'eng_all': eng_all,
+				  'co2_loc_mean': co2_loc_mean, 'co2_loc': co2_loc, 'eng_loc_mean': eng_loc_mean, 'eng_loc': eng_loc}
+
+	return render(request, 'blog_details.html',context=result)
 
 def test(request):
 	# industry = request.GET.get('industry')
